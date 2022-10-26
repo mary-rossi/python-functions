fb16dfb9a557206e86551e4107d774186c7804db
Mary Rossi <maryrossi106@gmail.com>
2022-10-26 16:18:44 -0500

    exercise 4

--- python_functions.py
+++ python_functions.py
@@ -13,3 +13,21 @@ def largest(numbers_list):
 
 def occurrences(str1, str2):
    return str1.count(str2)
+
+
+def product(*numbers):
+   prod = 1
+
+   for num in numbers:
+      prod *= num
+   
+   return prod
+
+
+print(sum_to(10))
+
+print(largest([2, -5, 28, 16]))
+
+print(occurrences("chicky-pea, chicky-dee, supa free", "chi"))
+
+print(product(-3, 2, 10))
